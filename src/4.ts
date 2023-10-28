ba2a33e93b2f3cb1993fc447f7e531c0ad44b0be
Seven <graf.lancelot7@gmail.com>
2023-10-28 22:37:34 +0300

    add

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,52 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
+class Key {
+    constructor(private signature: number = Math.random()) {}
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    constructor(private key: Key) {}
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false;
+  
+    constructor(protected key: Key) {}
+  
+    private tenants: Person[] = [];
+  
+    public comeIn(person: Person): void {
+      if (this.door === true) {
+        this.tenants.push(person);
+      }
+    }
+  
+    public abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    public openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+      }
+    }
+  }
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  
+  house.comeIn(person);
+  
 
 export {};
